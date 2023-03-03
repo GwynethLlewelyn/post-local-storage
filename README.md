b5f4b6c195009347733fe0e5456e2575e60e7047
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2023-03-03 17:18:40 +0000

    Prettifying and adding Codacy
    Also, badges.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 
 A very simple phpBB3 extension to locally store the content of a post being written, to avoid losing everything after a crash or session expiration.
 
-Logo designed by DALL·E 2.
+Logo designed by [DALL·E 2](https://openai.com/product/dall-e-2).
 
+![phpBB3 Logo](https://img.shields.io/badge/phpBB-3.3-blue) [![CodeQL](https://github.com/GwynethLlewelyn/post-local-storage/actions/workflows/codeql.yml/badge.svg)](https://github.com/GwynethLlewelyn/post-local-storage/actions/workflows/codeql.yml) [![Codacy Badge](https://app.codacy.com/project/badge/Grade/83a20d04433341baa65c78d29fc3410a)](https://www.codacy.com/gh/GwynethLlewelyn/post-local-storage/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=GwynethLlewelyn/post-local-storage&amp;utm_campaign=Badge_Grade)
--- gwynethllewelyn/postlocalstorage/styles/prosilver/template/custom_functions.js
+++ gwynethllewelyn/postlocalstorage/styles/prosilver/template/custom_functions.js
@@ -2,16 +2,16 @@
 	// Cut the mustard.
 	if (!message.localStorage) return;
 	// You should probably use a more specific selector than this.
-	var textarea = doc.querySelector('textarea');
+	var textarea = doc.querySelector("textarea");
 	// The key for the key/value pair in localStorage is the current URL.
 	var key = message.location.href;
 	var item = null;
 	// Use the 'pagehide' event in modern browsers or 'beforeunload' in older browsers.
 	var unloadEvent;
-	if ('onpagehide' in message) {
-		unloadEvent = 'pagehide';
+	if ("onpagehide" in message) {
+		unloadEvent = "pagehide";
 	} else {
-		unloadEvent = 'beforeunload';
+		unloadEvent = "beforeunload";
 	}
 	// If there's a localStorage entry for the current URL, update the textarea with the saved value.
 	item = message.localStorage.getItem(key);
@@ -22,7 +22,7 @@
 	// This function will store the current value of the textarea in localStorage (or delete it if the textarea is blank).
 	function updateStorage() {
 		if (textarea.value) {
-			item = JSON.stringify({'content': textarea.value});
+			item = JSON.stringify({ content: textarea.value });
 			message.localStorage.setItem(key, item);
 		} else {
 			message.localStorage.removeItem(key);
@@ -31,13 +31,17 @@
 		message.removeEventListener(unloadEvent, updateStorage);
 	}
 	// When the user presses a key just *once* inside the textarea, run the storage function when the page is unloaded.
-	textarea.addEventListener('keyup', function() {
-		message.addEventListener(unloadEvent, updateStorage);
-		message.setInterval(updateStorage, 10000);
-	}, {'once': true});
+	textarea.addEventListener(
+		"keyup",
+		function () {
+			message.addEventListener(unloadEvent, updateStorage);
+			message.setInterval(updateStorage, 10000);
+		},
+		{ once: true }
+	);
 	// When the form is submitted, delete the localStorage key/value pair.
-	textarea.form.addEventListener('submit', function() {
+	textarea.form.addEventListener("submit", function () {
 		message.localStorage.removeItem(key);
 		message.removeEventListener(unloadEvent, updateStorage);
 	});
-}(this, this.document));
+})(this, this.document);
