22546a0b79ac83e1c21eecb5e1fb8e3b9dc4ec43
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2024-04-20 19:20:42 +0100

    Fix: remove constructor It breaks things!

--- gwynethllewelyn/postlocalstorage/ext.php
+++ gwynethllewelyn/postlocalstorage/ext.php
@@ -19,25 +19,7 @@ use phpbb\extension\base;
  */
 class ext extends base
 {
-	/**
-	 * Class constructor, just to let us know that we reached this point.
-	 *
-	 * @param ContainerInterface $container Container object
-	 * @param \phpbb\finder $extension_finder
-	 * @param \phpbb\db\migrator $migrator
-	 * @param string $extension_name Name of this extension (from ext.manager)
-	 * @param string $extension_path Relative path to this extension
-	 */
-	public function __construct(ContainerInterface $container, \phpbb\finder $extension_finder, \phpbb\db\migrator $migrator, $extension_name, $extension_path)
-	{
-		$this->container = $container;
-		$this->extension_finder = $extension_finder;
-		$this->migrator = $migrator;
 
-		$this->extension_name = $extension_name;
-		$this->extension_path = $extension_path;
-		error_log('[phpBB3 postlocalstorage] my base constructor was called!');
-	}
 
 	/**
 	 * Check whether or not the extension can be enabled.
@@ -46,6 +28,7 @@ class ext extends base
 	 */
 	public function is_enableable()
 	{
-			return phpbb_version_compare(PHPBB_VERSION, '3.3', '>=');
+		error_log('[phpBB3 postlocalstorage] my is_enableable() was called!');
+		return phpbb_version_compare(PHPBB_VERSION, '3.3', '>=');
 	}
 }
