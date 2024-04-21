d5a503f4467f04117bd4a4532b4264638117bdb6
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2024-04-21 21:53:12 +0100

    Chore: make sure ESLint has config file Or else, it throws a stupid error and blocks on my editor!

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// eslint 9.1.0 config file
+module.exports = [
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "script"
+		},
+		files: ["**/*.js"],
+		ignores: ["**/.*", ".*/*"]
+	}
+];
