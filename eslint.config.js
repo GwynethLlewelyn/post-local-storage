344d481cb7b4cd08ddb3c3363a5717012166bb67
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2024-04-23 20:08:35 +0100

    Fix: command was giving a stupid error

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 // eslint 9.1.0 config file
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 module.exports = [
 	{
@@ -8,6 +7,6 @@ module.exports = [
 			sourceType: "script"
 		},
 		files: ["**/*.js"],
-		ignores: ["**/.*", ".*/*"]
+		ignores: ["**/.*", ".*/*"],
 	}
 ];
